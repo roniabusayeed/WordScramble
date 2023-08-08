aba2bed87adf31c02d59ae046a3bd2bfea730e16
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-08 18:19:46 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Abu Sayeed Roni on 2023-08-08.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
