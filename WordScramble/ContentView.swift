91da8c668cff7742d712aabedf51cab8467b5a48
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-10 00:27:08 -0300

    Add word to the list

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rootWord: String = "Rootword"
+    @State private var newWord: String = ""
+    @State private var usedWords: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) {word in
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .navigationBarTitleDisplayMode(.inline)
+            .onSubmit(addNewWord)
         }
-        .padding()
+    }
+    
+    func addNewWord() {
+        
+        // Normalize input word.
+        let word = newWord.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+        
+        // Validation.
+        let isNotAnEmptyWord = word.count > 0
+        let isOneWord = word.components(separatedBy: .whitespacesAndNewlines).count == 1
+        guard isNotAnEmptyWord && isOneWord else { return }
+        
+        // Add the word to the used word list.
+        withAnimation {
+            usedWords.insert(word, at: 0)
+        }
+        
+        // Clear new word text field for the next word entry.
+        newWord.removeAll()
     }
 }
 
